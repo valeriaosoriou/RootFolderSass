eb64adc7d3a3761cbacbdd7ea27cdfcccf92f189
Val OSORIO UBILLO <270022830@bun2-204-12.local>
2019-09-19 23:17:01 +1200

    menu added

---
+++ js/sidebar.js
@@ -0,0 +1,13 @@
+/*$(document).ready(function() {
+    var hamburger = $('.header-menu_hamburger');
+    var body = $('body');
+
+    hamburger.click(function() {
+        if (body.hasClass('open-sidebar')) {
+            body.removeClass('open-sidebar');
+        } else {
+            body.addClass('open-sidebar');
+        }
+    })
+
+})
